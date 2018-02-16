2c68e04d691205da42ebadfb20dd09b58b64ff97
vmischeryakov <vmischeryakov@gmail.com>
2018-02-16 17:42:54 +0200

    Remove unused constants.

--- src/main/java/lamao/soh/SHConstants.java
+++ src/main/java/lamao/soh/SHConstants.java
@@ -18,20 +18,6 @@ import java.util.List;
  */
 public class SHConstants
 {
-	public static final int BUILD_NUMBER = 1;
-	
-	// Entity names. Defines names of resources in res manager, in
-	// configuration files and obj-files
-	public static final String LEFT_WALL = "left-wall";
-	public static final String TOP_WALL = "top-wall";
-	public static final String RIGHT_WALL = "right-wall";
-	public static final String BOTTOM_WALL = "bottom-wall";
-	public static final String BRICK = "brick";
-	public static final String BALL = "ball";
-	public static final String PADDLE = "paddle";
-	public static final String PADDLE_GUN = "paddle-gun-model";
-	public static final String DECORATION = "decoration";
-	public static final String BULLET = "bullet";
     public static List<String> PATHS_TO_ASSETS = Arrays.asList("data", "assets");
 
 	/** Directory where player's profiles are stored */
